f6f99c45a1b41a8b4af9449b5ad1678a7381d243
Andy Rothstein <arothste@caltech.edu>
2020-05-17 22:21:11 -0700

    Updated return velocity test simulations

--- period_test.m
+++ period_test.m
@@ -14,7 +14,7 @@ mhu = 398600; %Gravitational constant of Earth in km^3/s^2
 Re = 6378; % Radius of Earth  in km
 
 % User edited variables
-N = 10000;                      % Number of satellites
+N = 50000;                      % Number of satellites
 center = [0,0,0];           % Location of chief (m)
 mind = 15;                  % min distance from center (m)
 maxd = 40;                 % max distance from center (m)
@@ -35,7 +35,7 @@ period = 2*pi*sqrt(a_chief^3/mhu); %period of the chief orbit in seconds
 n = 2*pi/period; % Mean motion in radians/second
 
 %% Relaxes restrictions on PRO search for x'(0) and z'(0)
-
+%{
 k = 1;
 dv_total = 0;
 f = waitbar(0,"Loading");
@@ -62,38 +62,76 @@ while k <= N
 end
 close(f);
 disp("Average delta v (m/s): "+dv_total/N);
-
+%}
 %% Propagates a random IC forward looking for recovergence period
-% Also counts how many will return in <2 days
 
-time = 1:10:t_max;
-returns = zeros(N, 1);
 loop = 2*pi*a_chief;
-f = waitbar(0,"Loading");
-for j = 1:N
-    t = 24*60*60/10;
-    states = [];
-    while t < length(time)
-        states(1:6) = exppA_state(n,time(t))*state_ejec(j,:)';
-        if abs(states(2)) > loop/2
-            states(2) = mod(states(2),loop);
+tinits = [2 12 24 36 48].*60.*6;
+tmax = (1:7).*24.*60.*6;
+t0 = 1;
+fileID = fopen('return_data.txt','w');
+while t0 <= length(tinits)
+    tend = 1;
+    while tend <= length(tmax)
+        while tinits(t0) >= tmax(tend)
+            tend = tend + 1;
         end
-        if states(2) > loop/2
-            states(2) = states(2) - loop;
+        returns = zeros(N, 1);
+        dv = zeros(N,1);
+        f = waitbar(0,"Loading");
+        for j = 1:N
+            t = tinits(t0);
+            states = [];
+            while t < tmax(tend)
+                states(1:6) = exppA_state(n,t*10)*state_ejec(j,:)';
+                if abs(states(2)) > loop/2
+                    states(2) = mod(states(2),loop);
+                end
+                if states(2) > loop/2
+                    states(2) = states(2) - loop;
+                end
+
+                if norm(states(1:3)) < 200
+                    returns(j) = t*10;
+                    t = t + tmax(tend);
+                else
+                    t = t + 1;
+                end
+            end
+            if returns(j) == 0
+                vy = state_ejec(j,5);
+                vy_return = vy*tinits(t0)/(tmax(tend)-tinits(t0));
+                dv(j) = abs(vy) + vy_return;
+            end
+            if mod(j,N/100) == 0
+                waitbar(j/N, f, 100*j/N+"% Done with start "+tinits(t0)/360+"hr, end "+tmax(tend)/(24*360)+"days");
+            end
         end
-        
-        if norm(states(1:3)) < 500
-            returns(j) = t*10;
-            t = t + length(time);
-        else
-            t = t + 1;
-        end
+        t1 = "Start: "+tinits(t0)/360+"hr, end: "+tmax(tend)/(24*360)+"days";
+        t2 = "Returns with no firing: "+sum(returns > 0)*100/N+"%";
+        t3 = "Avg Delta v of firing Cubesats (m/s): "+sum(dv)/sum(dv>0);
+        t4 = "Avg Delta v over all Cubesats  (m/s): "+sum(dv)/N;
+        t5 = "Max Delta v impulse            (m/s): "+max(dv);
+        endline = "\r\n";
+        disp(t1);
+        disp(t2);
+        disp(t3);
+        disp(t4);
+        disp(t5);
+        disp(" ");
+        fprintf(fileID, t1+endline);
+        fprintf(fileID, t2+endline);
+        fprintf(fileID, t3+endline);
+        fprintf(fileID, t4+endline);
+        fprintf(fileID, t5+endline);
+        fprintf(fileID, endline);
+        close(f);
+        tend = tend + 1;
     end
-    if mod(j,N/100) == 0
-        waitbar(j/N, f, 100*j/N+"% Done");
-    end
+    t0 = t0 + 1;
 end
-close(f);
+fclose(fileID);
+%{
 count2 = 0;
 count3 = 0;
 count4 = 0;
@@ -117,6 +155,7 @@ disp("Return <4 day percentage: "+100*count4/N);
 disp("Average return time: "+mean(returns)/3600+"hr");
 disp("STD: "+std(returns)/3600+"hr");
 disp("Max return time: "+max(returns)/3600/24+"days");
+%}
 %%
 % Plots ejection orbits
 if plt
